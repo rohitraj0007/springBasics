fb364c02fd3cebc8c9d15d00e2dc4b41beb82a69
rohitraj0007 <44340474+rohitraj0007@users.noreply.github.com>
2022-08-02 14:46:48 +0530

    propert read

--- spring-demo-one/src/com/senseOfCode/springDemo/BaseBallCoach.java
+++ spring-demo-one/src/com/senseOfCode/springDemo/BaseBallCoach.java
@@ -6,6 +6,7 @@ public class BaseBallCoach implements Coach{
 	//define a private field for the dependency
 		private FortuneService fortuneService;
 		private String team;
+		private String email;
 		
 		//define a default constructor 
 		public BaseBallCoach() {
@@ -19,10 +20,14 @@ public class BaseBallCoach implements Coach{
 		public void setTeam(String team) {
 			this.team = team;
 		}
+		
+		public void setemail(String email) {
+			this.email = email;
+		}
 
 @Override
 public String getDailyWorkout() {
-	return "spend 30 minutes on baseBall team: "+team;
+	return "spend 30 minutes on baseBall team: "+team +" "+ email;
 }
 
 @Override
@@ -31,4 +36,5 @@ public String getDailyFortune() {
 	return fortuneService.getFortune()+"setter injection";
 
 }
+
 }
